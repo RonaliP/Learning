1411797a8b16278afdc0ac171dd283b55efd4ade
Ronali Panigrahy <ronalipanigrahy88@gmail.com>
2021-04-14 16:57:18 +0530

    used templates to render html files on webpage

--- Template.py
+++ Template.py
@@ -1,20 +1,11 @@
-from flask import Flask,redirect,url_for
+from flask import Flask,redirect,url_for,render_template
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def home():
-    return "Hello this is the first page using flask <h1>HELLO FLASK</h1>"
-
-@app.route("/<name>")
-def user(name):
-    return f"hello {name}::::"
-
-
-@app.route("/admin")
-def admin():
-    return redirect(url_for("user",Name="Admin"))
+    return render_template("index.html")
 
 if __name__ == "__main__":
     app.run(debug=True)

30bd7fbf8ee1e2dd226e61faf8827f6ec76d88a9
Ronali Panigrahy <ronalipanigrahy88@gmail.com>
2021-04-14 19:43:45 +0530

    passing content through arguement

--- Template.py
+++ Template.py
@@ -1,11 +1,11 @@
 from flask import Flask,redirect,url_for,render_template
 
-app = Flask(__name__)
+app = Flask(__name__,template_folder="templates")
 
 
 @app.route("/<name>")
 def home(name):
-    return render_template("index.html")
+    return render_template( "index.html",content=name )
 
 if __name__ == "__main__":
     app.run(debug=True)
